ccf9511443e0a2c4f27f755d6bf719e203fe881c
ituitis-beydemir18 <alpbeydemir@hotmail.com>
2023-12-23 21:53:11 +0300

    correction in  adjecency districts table

--- templambda2.py
+++ templambda2.py
@@ -1,23 +1,38 @@
 import boto3
 
-def get_unique_districts(table_name):
-    # DynamoDB client'ını başlat
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(table_name)
 
-    # Tüm öğeleri almak için scan yap
+# DynamoDB'ye bağlantı
+dynamodb = boto3.resource('dynamodb')
+
+# Verilen string listesi
+district_list =['ATAŞEHİR', 'KADIKÖY', 'ÜSKÜDAR', 'FATİH', 'KARTAL', 'BAŞAKŞEHİR', 'TUZLA', 'BEYOĞLU', 'KAĞITHANE', 'SULTANBEYLİ', 'GÜNGÖREN', 'SARIYER', 'AVCILAR', 'MALTEPE', 'BAKIRKÖY', 'ŞİŞLİ', 'BAYRAMPAŞA', 'ÇEKMEKÖY', 'ARNAVUTKÖY', 'ÜMRANİYE', 'BEYLİKDÜZÜ', 'GAZİOSMANPAŞA', 'KÜÇÜKÇEKMECE', 'EYÜP', 'BEYKOZ', 'ZEYTİNBURNU', 'PENDİK', 
+'ÇATALCA', 'BÜYÜKÇEKMECE', 'BEŞİKTAŞ', 'BAĞCILAR', 'SİLİVRİ', 'ESENLER', 'SULTANGAZİ', 'BAHÇELİEVLER']
+
+def check_districts(dynamodb, district_list):
+    """
+    DynamoDB'deki Districts tablosundan tüm item'ları alır ve verilen listeye göre kontrol eder.
+    """
+    table = dynamodb.Table('Districts')
     response = table.scan()
     items = response['Items']
 
-    # Benzersiz ilçeleri bir set içinde sakla
-    unique_districts = set()
+    missing_districts = []
+
     for item in items:
-        district = item['district']
-        unique_districts.add(district)
+        district_name = item['Name']
+        adjacent_districts = [adj for adj in item['AdjacentDistricts']]
 
-    # Benzersiz ilçeleri ve sayılarını yazdır
-    print("Unique Districts:", unique_districts)
-    print("Total Number of Districts:", len(unique_districts))
+        if district_name not in district_list:
+            missing_districts.append(district_name)
 
-# Fonksiyonu çağır
-get_unique_districts('Parks_cache')
+        for adj_district in adjacent_districts:
+            if adj_district not in district_list:
+                missing_districts.append(adj_district)
+
+    return list(set(missing_districts))  # Benzersiz eksik ilçeleri döndür
+
+# Eksik ilçeleri kontrol et
+missing_districts = check_districts(dynamodb, district_list)
+print(missing_districts)
+
+
